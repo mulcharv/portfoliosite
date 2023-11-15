400488531504df715cd241213b6f37f68b66cb39
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2023-11-15 10:35:03 +0000

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,18 +9,15 @@ export default function About() {
                 <div className="aboutdescfirst">
                 Hello! My name is Rahul and I am a passionate software developer who enjoys creating full stack web applications.
 
-                Working in accounting and tax after a Bachelor of Mechanical Engineering, I started to see the huge potential for more efficient and 
-                productive solutions that can help people and businesses save and grow their wealth.
+                Working in public accounting and tax after a Bachelor of Mechanical Engineering, I started to see the huge potential for technology to create 
+                more efficient and productive solutions that can help a variety of people and businesses.
                 </div>
                 <div className="aboutdescpics">
                     <img className="aboutdescimg" src={'/accountingpic.jpg'} alt=''></img>
                     <img className="aboutdescimg" src={'/engineeringpic.jpg'} alt=''></img>
                 </div>
                 <div className="aboutdescsecond">
-                 I decided to leverage the knowledge of reporting financial data
-                and the problem solving mindset of an engineer into learning programming for a software developer role in a Fintech firm. 
-                
-                This prompted a move to London for its industry leading Fintech scene, where I hope to contribute to a solution that can help users financially in a meaningful way.
+                This led me on a self-taught journey of full-stack software development. I hope to use these skills to contribute to a firm that creates meaningful software solutions.
                 </div>
             </div>
         </div>
